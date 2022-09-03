5fac49a8501a3ca4ff639f57b0e26dfc8ff0c07e
Maria Sanzhanova <smo_0207@mail.ru>
2022-09-03 18:43:48 +0500

    Main and README.md were added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 70, 100, 300, 200};
+        SalesManager salesManager = new SalesManager(arr);
+
+        int max = salesManager.max();
+
+        System.out.println(max);
+    }
+}
